8bdb6d70aad2e6bcd1057f072708569c2de5ae17
Hemil patel <hemilp@mcm.com>
2016-03-15 16:28:08 -0700

    sdf
    sdf

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+
+
+angular.module('bmcProj', [])
+  .controller('BmcProjController', function($scope) {
+
+      $scope.people = [
+        {name: 'Allen Allbrook', email:'A.Allbrook@calbroswervice.com'},
+        {name: 'Alien Border' , email:'aborder@Petramco.com'}
+      ]
+
+   });
